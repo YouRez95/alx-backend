ffb1e845cc0b56331661109d00e1f66d2f52147b
Youness Elalouani <younesselalouani1995@gmail.com>
2024-05-10 13:43:31 +0100

    fix lru cache

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -18,39 +18,44 @@ class LRUCache(BaseCaching):
             Inherit the cache data from parent class
         '''
         super().__init__()
-        self.tracking = []
+        self.tracking = deque()
 
     def put(self, key, item):
         '''
             Add an item to the cache
         '''
-        if not key or not item:
-            return
-        if self.MAX_ITEMS == len(self.tracking):
-            evicted = self.tracking[0]
-            del self.cache_data[evicted]
-            if key not in self.tracking:
-                print('DISCARD: {}'.format(evicted))
-            for i in range(0, self.MAX_ITEMS):
-                if i + 1 < self.MAX_ITEMS:
-                    self.tracking[i] = self.tracking[i + 1]
-                else:
-                    self.tracking[i] = key
+        if key in self.cache_data:
             self.cache_data[key] = item
-        else:
-            self.cache_data[key] = item
-            self.tracking.append(key)
+            index = self.tracking.index(key)
+            self.swap(index)
+
+        if key not in self.cache_data:
+            if len(self.tracking) == self.MAX_ITEMS:
+                evicted_key = self.tracking.pop()
+                print("DISCARD: {}".format(evicted_key))
+                self.tracking.appendleft(key)
+                del self.cache_data[evicted_key]
+                self.cache_data[key] = item
+            else:
+                self.cache_data[key] = item
+                self.tracking.appendleft(key)
 
     def get(self, key):
         '''
             Get an item from the cache
         '''
-        if not key or key not in self.cache_data:
+        if key not in self.tracking:
             return None
-        # self.tracking.
-        index = self.tracking.index(key)
-        for i in range(index, self.MAX_ITEMS):
-            if i + 1 < self.MAX_ITEMS:
-                self.tracking[i] = self.tracking[i + 1]
-        self.tracking[self.MAX_ITEMS - 1] = key
+        else:
+            index = self.tracking.index(key)
+            self.swap(index)
+        # print(self.tracking)
         return self.cache_data[key]
+
+    def swap(self, index):
+        """
+            swap the tracking list
+        """
+        for i in range(index, 0, -1):
+            self.tracking[i], self.tracking[i - 1] = \
+                self.tracking[i - 1], self.tracking[i]
