6524d0025612cc7a9b257a94c325cf3cd7460f4b
Youness Elalouani <younesselalouani1995@gmail.com>
2024-05-14 11:10:06 +0100

    add types

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -5,17 +5,17 @@
 
 
 from flask import Flask, render_template
-from flask.typing import ResponseReturnValue
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def home_route() -> ResponseReturnValue:
+def home_route() -> str:
     '''
         function that serve the route /
     '''
     return render_template('0-index.html')
 
 
-app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
