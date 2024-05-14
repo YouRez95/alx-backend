a00232bda6a8541dd46efe2d35ea22b1615a46fa
Youness Elalouani <younesselalouani1995@gmail.com>
2024-05-14 12:19:03 +0100

    add comment

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -24,6 +24,9 @@ babel = Babel(app)
 
 @babel.localeselector
 def get_locale() -> str:
+    '''
+        Retrieve the locale
+    '''
     request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
