185d25ab6847597e32415cebabab40b93e3f26dc
Youness Elalouani <younesselalouani1995@gmail.com>
2024-05-14 11:39:56 +0100

    basic babel setup

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+'''
+    Basic Babel setup
+'''
+
+
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+class Config:
+    '''
+        config class for the flask app
+    '''
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@app.route('/')
+def home_route() -> str:
+    '''
+        function that serve the route /
+    '''
+    return render_template('0-index.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
