1fba460efca1f15ec61a461d989ad670079d1917
Youness Elalouani <younesselalouani1995@gmail.com>
2024-05-14 12:18:07 +0100

    add init app

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -23,11 +23,8 @@ babel = Babel(app)
 
 
 @babel.localeselector
-def get_locale():
+def get_locale() -> str:
     request.accept_languages.best_match(app.config['LANGUAGES'])
-
-
-# babel.init_app(app, locale_selector=get_locale)
 
 
 @app.route('/')
