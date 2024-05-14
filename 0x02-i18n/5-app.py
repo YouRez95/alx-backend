f15e1985d9a68e06c5415ea1c95d092e06b627ad
Youness Elalouani <younesselalouani1995@gmail.com>
2024-05-14 19:49:09 +0100

    Mock logging in

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -4,7 +4,7 @@
 '''
 
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, g
 from flask_babel import Babel
 
 
@@ -22,13 +22,34 @@ app.config.from_object(Config)
 babel = Babel(app)
 
 
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+def get_user(user_id, users):
+    if user_id and int(user_id) in users.keys():
+        return users[int(user_id)]
+    return None
+
+
+@app.before_request
+def before_request():
+    user_id = request.args.get('login_as')
+    user = get_user(user_id, users)
+    g.user = user
+
+
 @babel.localeselector
 def get_locale() -> str:
     '''
         Retrieve the locale
     '''
     local = request.args.get('locale')
-    if '|' in local:
+    if local and '|' in local:
         local = local[1:-1].split('|')
         for lang in local:
             if lang in app.config['LANGUAGES']:
@@ -37,6 +58,7 @@ def get_locale() -> str:
     if local in app.config['LANGUAGES']:
         return local
     request.accept_languages.best_match(app.config['LANGUAGES'])
+# babel.init_app(app, locale_selector=get_locale)
 
 
 @app.route('/')
@@ -44,7 +66,7 @@ def home_route() -> str:
     '''
         function that serve the route /
     '''
-    return render_template('4-index.html')
+    return render_template('5-index.html')
 
 
 if __name__ == "__main__":
